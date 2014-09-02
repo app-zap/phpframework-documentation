b2ca3d83338c596d7e70873b7740170164a0de81
Sebastian Michaelsen <sebastian@app-zap.de>
2014-09-02 12:00:35 +0200

    :wrench: Make default application options configurable
    The IniParser sets a few default variables that now can be overwritten
    in your settings.ini. These include:
    
    [application]
    application =
    application_directory =
    migration_directory =
    routes_files =
    template_directory =

--- classes/Configuration/Parser/IniParser.php
+++ classes/Configuration/Parser/IniParser.php
@@ -21,12 +21,12 @@ class IniParser {
     if (is_null($overwrite_file_path)) {
       $overwrite_file_path = $application_directory . 'settings_local.ini';
     }
-    self::parse($config_file_path, $overwrite_file_path);
     Configuration::set('application', 'application', $application);
     Configuration::set('application', 'application_directory', $application_directory);
     Configuration::set('application', 'migration_directory', $application_directory . '_sql/');
     Configuration::set('application', 'routes_file', $application_directory . 'routes.php');
     Configuration::set('application', 'templates_directory', $application_directory . 'templates/');
+    self::parse($config_file_path, $overwrite_file_path);
   }
 
   /**
